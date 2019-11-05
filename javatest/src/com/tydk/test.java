12fb989d4772c778721f4b809cb306b6497797dd
Lgh520 <lghao417@163.com>
2019-11-05 15:30:35 +0800

    1

--- javatest/src/com/tydk/test.java
+++ javatest/src/com/tydk/test.java
@@ -1,9 +1,16 @@
 package com.tydk;
 
+import jdk.internal.util.xml.impl.Input;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
 public class test {
     public static void main(String[] args) {
         System.out.println("hellolll");
         System.out.println("helloabc");
+        Thread t=new Thread();
     }
 
 }
